b8f54477e20be95f0b0d6cb759b7557a13c9bf0d
sarahhae <helloitssarahhae@gmail.com>
2020-11-09 12:10:59 +1300

    added chat components

---
+++ src/components/Chat.js
@@ -0,0 +1,5 @@
+// Chat main - chat function, render message, create new chat
+// Chat body - user to user chat
+// Chat input - Form, onclick button send realtime message
+// Display new/old messages on realtime
+// Display notification icon
---
+++ src/components/ChatNewMatches.js
@@ -0,0 +1,2 @@
+// Display new matches on ChatList
+// Function - onClick avatar, open new chat window

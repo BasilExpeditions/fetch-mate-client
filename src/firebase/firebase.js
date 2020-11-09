2e7c3389ca7f9b3eabe0667fbbaa56ff7044961e
Bastien Durand <basilexpeditions@gmail.com>
2020-11-10 10:56:07 +1100

    errors for now

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,62 +1,62 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-
-const config = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  databaseURL: process.env.REACT_APP_DB,
-  projectId: process.env.REACT_APP_PID,
-  storageBucket: process.env.REACT_APP_SB,
-  messagingSenderId: process.env.REACT_APP_SID,
-  appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MID,
-};
-
-const provider = new firebase.auth.GoogleAuthProvider();
-
-export const generateUserDocument = async (user, additionalData) => {
-  if (!user) return;
-
-  const userRef = firestore.doc(`users/${user.uid}`);
-
-  const snapshot = await userRef.get();
-
-  if (!snapshot.exists) {
-    const { email, displayName, photoURL } = user;
-
-    try {
-      await userRef.set({
-        displayName,
-        email,
-        photoURL,
-        ...additionalData,
-      });
-    } catch (error) {
-      console.error("Error creating user document", error);
-    }
-  }
-
-  return getUserDocument(user.uid);
-};
-
-const getUserDocument = async (uid) => {
-  if (!uid) return null;
-  try {
-    const userDocument = await firestore.doc(`users/${uid}`).get();
-    return {
-      uid,
-      ...userDocument.data(),
-    };
-  } catch (error) {
-    console.error("Error fetching user", error);
-  }
-};
-
-firebase.initializeApp(config);
-
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
-export const signInWithGoogle = () => {
-  auth.signInWithPopup(provider);
-};
+// import firebase from "firebase/app";
+// import "firebase/auth";
+// import "firebase/firestore";
+//
+// const config = {
+//   apiKey: process.env.REACT_APP_APIKEY,
+//   authDomain: process.env.REACT_APP_AUTHDOMAIN,
+//   databaseURL: process.env.REACT_APP_DB,
+//   projectId: process.env.REACT_APP_PID,
+//   storageBucket: process.env.REACT_APP_SB,
+//   messagingSenderId: process.env.REACT_APP_SID,
+//   appId: process.env.REACT_APP_APPID,
+//   measurementId: process.env.REACT_APP_MID,
+// };
+//
+// const provider = new firebase.auth.GoogleAuthProvider();
+//
+// export const generateUserDocument = async (user, additionalData) => {
+//   if (!user) return;
+//
+//   const userRef = firestore.doc(`users/${user.uid}`);
+//
+//   const snapshot = await userRef.get();
+//
+//   if (!snapshot.exists) {
+//     const { email, displayName, photoURL } = user;
+//
+//     try {
+//       await userRef.set({
+//         displayName,
+//         email,
+//         photoURL,
+//         ...additionalData,
+//       });
+//     } catch (error) {
+//       console.error("Error creating user document", error);
+//     }
+//   }
+//
+//   return getUserDocument(user.uid);
+// };
+//
+// const getUserDocument = async (uid) => {
+//   if (!uid) return null;
+//   try {
+//     const userDocument = await firestore.doc(`users/${uid}`).get();
+//     return {
+//       uid,
+//       ...userDocument.data(),
+//     };
+//   } catch (error) {
+//     console.error("Error fetching user", error);
+//   }
+// };
+//
+// firebase.initializeApp(config);
+//
+// export const auth = firebase.auth();
+// export const firestore = firebase.firestore();
+// export const signInWithGoogle = () => {
+//   auth.signInWithPopup(provider);
+// };

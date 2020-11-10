4799558038f269036372a42da598556cb2d5395c
Ryan Bullough <rjbullough@gmail.com>
2020-11-10 17:33:39 +1100

    Bastien said it's really weird

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,27 @@
 import React, { useContext } from "react";
 import { auth } from "../firebase/firebase";
 import { UserContext } from "../providers/UserProvider";
-import SaveCurrentLocation from "./navigation/SaveCurrentLocation";
-
+import SwipeCard from "./SwipeCard";
 const Home = () => {
-  SaveCurrentLocation();
   const user = useContext(UserContext);
   return (
     <div>
-      <h1>Welcome {user.displayName}</h1>
-      <button
-        onClick={() => {
-          auth.signOut();
-        }}
-      >
-        Sign out
-      </button>
+      <div className="container">
+        <div className="child">
+          <h4>
+            Welcome {user.displayName}
+            <button
+              onClick={() => {
+                auth.signOut();
+              }}
+            >
+              Sign out
+            </button>
+          </h4>
+          <SwipeCard />
+        </div>
+      </div>
     </div>
   );
 };
-
 export default Home;
